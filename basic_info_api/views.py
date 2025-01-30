a3b650fdc6983292db6c1a287756d294b0c7c904
Mohammad Omar <mohaomar495@gmail.com>
2025-01-30 11:49:53 +0300

    Update views.py

--- basic_info_api/views.py
+++ basic_info_api/views.py
@@ -5,6 +5,6 @@ def get_info(request):
     response_data = {
             "email":"mohaomar495@gmail.com",
             "current_datetime": datetime.utcnow().isoformat() + "Z",
-            "github_url": "https://github.com/mohaomar495/HNG12-BackEnd-Internship/"
+            "github_url": "https://github.com/mohaomar495/HNG12-Backend-Task0"
             }
     return JsonResponse(response_data)
